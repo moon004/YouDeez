aeed8e9aade24e51c436cf63ed3d8bd32dd1cbb2
moon004 <herodotus94@gmail.com>
2018-12-05 12:41:24 +0800

    Added test for Deezer

--- app/src/Test/main_test.go
+++ app/src/Test/main_test.go
@@ -1,17 +1,16 @@
 package main_test
 
 import (
-	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
-	. "github.com/HopesAndDreams/app/src/handler"
+	. "github.com/hopesanddreams/app/src/handler"
 )
 
-func TestGetYtube(t *testing.T) {
-	var DoggoRes DogResources
+func TestGetDeezer(t *testing.T) {
+	var Deez DeezResource
 	tt := []struct {
 		name   string //Test name
 		value  string // Pass in value
@@ -19,21 +18,20 @@ func TestGetYtube(t *testing.T) {
 		status int
 		err    string
 	}{
-		{name: "test /doggo", value: "doggo", status: 200},
-		{name: "Should error", value: "hahaha", err: "Unknown Route"},
-		{name: "test /youtube", value: "youtube?q=goose house", status: 200},
+		{name: "test /deezer", value: "api/deez?q=sad", status: 200},
+		{name: "test /deezer", value: "api/deez?q=goose house", status: 200},
+		{name: "Should error", value: "api/deez?q=2343424", err: "{}\n"}, //Empty cuz no result
 	}
-	dapi := &GetDoggoApi{}
 	for _, tc := range tt {
 		t.Run(tc.name, func(t *testing.T) {
-			req, err := http.NewRequest("POST", "localhost:8888/"+tc.value, nil)
+			req, err := http.NewRequest("GET", "localhost:8888/"+tc.value, nil)
 			if err != nil {
 				t.Fatalf("Error Request:%v", err)
 			}
 
 			rec := httptest.NewRecorder()
 
-			DoggoRes.GetDoggo(rec, req)
+			Deez.GetDeezTrack(rec, req)
 			res := rec.Result()
 			defer res.Body.Close()
 
@@ -44,22 +42,12 @@ func TestGetYtube(t *testing.T) {
 			bodyString := string(body)
 			//if tc is an error test
 			if tc.err != "" {
-				if res.StatusCode != http.StatusBadRequest {
-					t.Errorf("Expected bad status but got: %v", res.StatusCode)
-				}
+
 				// if given err msg is wrong
 				if msg := bodyString; msg != tc.err {
 					t.Errorf("expected Message %q, but got %q", tc.err, msg)
 				}
 				return //stop testing below's code
-			}
-
-			// if no err, get object from the json
-
-			json.Unmarshal(body, dapi)
-			if dapi.Status != "success" {
-				t.Errorf("expected Status %v, but got %v",
-					tc.status, res.StatusCode)
 			}
 
 		})
