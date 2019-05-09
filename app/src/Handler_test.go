435271606739f9dd35bd5a69f505cea73e5ed380
moon004 <herodotus94@gmail.com>
2019-05-09 18:43:24 +0800

    update test

--- app/src/Handler_test.go
+++ app/src/Handler_test.go
@@ -41,7 +41,7 @@ func TestGetDeezer(t *testing.T) {
 		{name: "test with space",
 			value:  "api/deez?q=goose house",
 			status: 200,
-			output: "Sing",
+			output: "Goose",
 		}, //		unknown ID
 		{name: "Empty Response", value: "api/deez?q=f439904f3f213", err: "{}\n"}, //Empty cuz no result
 	}
