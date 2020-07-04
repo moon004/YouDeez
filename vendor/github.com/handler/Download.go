cf5c7b2c8db315a8c4356a3273447ad996166316
HopesAndDreams <herodotus94@gmail.com>
2020-07-04 15:04:53 +0900

    Minor int to string 1

--- vendor/github.com/handler/Download.go
+++ vendor/github.com/handler/Download.go
@@ -94,7 +94,7 @@ func DeezExe(q, usertoken string, w http.ResponseWriter, r *http.Request) {
 	var cmd *exec.Cmd
 
 	if usertoken == "getsize" {
-		w.Write([]byte(1))
+		w.Write([]byte("1"))
 	} else {
 		cmd = exec.Command(
 			// REMEMBER ./deezer-downloader for linux
